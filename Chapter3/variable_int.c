08ea6926bae83238d4496cfbdb2046fd2ca97d41
jihye_heo <64202709+jihyeheo@users.noreply.github.com>
2021-08-02 17:39:26 +0900

    20210802

---
+++ Chapter3/variable_int.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d, e;
+	double da;	//double형 변수 da 선언
+	char ch;	// char형 변수 cc 선언
+
+	a = 10;
+	b = 5;
+	c = a + 2;
+	d = 33;
+	e = d - 22;
+	da = 3.5;
+	ch = 'a';
+
+
+
+	printf("변수 a 의 값 : %d\n", a);
+	printf("변수 b의 값 : %d\n", b);
+	printf("변수 da의 값 : %.1lf", da);
+	printf("변수 cc의 값 : %c\n", ch);
+
+
+
+
+}
