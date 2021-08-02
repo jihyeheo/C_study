2d19ac4f88db96283296d4bebe77d009116a599c
jihye_heo <64202709+jihyeheo@users.noreply.github.com>
2021-08-02 18:07:36 +0900

    20210802

---
+++ Chapter3/integer_variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	short sh = 12345;
+	int in = 23;
+	long ln = 333333333;
+	long long lln = 21732485985;
+
+	printf("short형 변수 출력 : %d\n", sh);
+	printf("int형 변수 출력 : %d\n", in);
+	printf("long형 변수 출력 : %ld\n", ln);
+	printf("long long형 변수 출력 : %lld\n", lln);
+
+
+	return 0;
+
+}
